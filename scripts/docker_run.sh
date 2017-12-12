f3012cced18b23c4b7509f91e0b48c95990cc988
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-12-12 22:42:57 +0100

    run coverage instead of make test

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -4,6 +4,5 @@ docker_build_dir="build_ubuntu_docker"
 
 docker run -ti -u `id -u $USER` $ci_env -e CC -e CXX -e GG_BUILD_TESTS --volume=$PWD:/home/ubuntu/shared bartekt/ubuntu-clang-cmake:latest\
     /bin/bash -c 'mkdir -p /tmp/build/ && cd /tmp/build && cmake /home/ubuntu/shared -DCMAKE_BUILD_TYPE=Debug -DGG_BUILD_TESTS=${GG_BUILD_TESTS} && make -j$(nproc) &&
-    if ['$GG_BUILD_TESTS' == 'ON']; then make test; fi && 
     if ['$GG_BUILD_TESTS' == 'ON']; then make coverage; fi && 
     if ['$GG_BUILD_TESTS' == 'ON']; then bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"; fi'
