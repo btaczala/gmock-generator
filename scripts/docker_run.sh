7877b17b8ee5935a9ca447df441324ecb27b6efd
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-12-13 11:10:43 +0100

    Use build directory

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -3,6 +3,6 @@
 docker_build_dir="build_ubuntu_docker"
 
 docker run -ti -u `id -u $USER` $ci_env -e CC -e CXX -e GG_BUILD_TESTS --volume=$PWD:/home/ubuntu/shared bartekt/ubuntu-clang-cmake:latest\
-    /bin/bash -c 'mkdir -p /tmp/build/ && cd /tmp/build && cmake /home/ubuntu/shared -DCMAKE_BUILD_TYPE=Debug -DGG_BUILD_TESTS=${GG_BUILD_TESTS} && make -j$(nproc) &&
+    /bin/bash -c 'cd /home/ubuntu/shared/ && mkdir -p build/ && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DGG_BUILD_TESTS=${GG_BUILD_TESTS} && make -j$(nproc) &&
     if [ '$GG_BUILD_TESTS' == 'ON' ]; then make test; fi && 
     if [ '$GG_BUILD_TESTS' == 'ON' ]; then bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"; fi'
